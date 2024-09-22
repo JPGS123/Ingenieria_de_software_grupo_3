892b214f4c6efe0f468a04238bad4f9745052748
gagalvezv <ga.galvezv@duocuc.cl>
2024-09-21 21:29:15 -0300

    Avance index

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('index/', index, name="index"),
+    path('', index, name='index'),  # Esta línea mapea la URL raíz a la vista index_view
 ]
