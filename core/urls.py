194feb4754ac2f4b944d323c5020e1c1643c6ef0
gagalvezv <ga.galvezv@duocuc.cl>
2024-09-22 19:53:09 -0300

    Se crea vista login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, eventos
+from .views import index, eventos, login
 
 urlpatterns = [
     path('', index, name='index'),
     path('eventos/', eventos, name='eventos'),
+    path('login/', login, name='login'),
 ]
